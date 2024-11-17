e9a5fe440990b6b76067941afe6013ea76b14222
Nikita <nikita@s-i-r-k-a.com>
2024-11-18 00:41:43 +0100

    fix(swagger docs config): auto define local server port with variable.
    Resolved issues with incorrect assignment of port and path from environment variables to swagger
    docs config object.

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ import app from './app';
 // Default dev port or container port.
 const PORT                   = Number(process.env.PORT) || 8080;
 // Port for queries outside the container.
-const HOST_PORT              = Number(process.env.HOST_PORT); 
+const HOST_PORT              = Number(process.env.HOST_PORT);
+// Live server URL for swagger
+const LIVE_SERVER_URL        = process.env.LIVE_SERVER_URL || 'Undefined. Read template docs for more info.';
 // Defined only in docker-compose.yml file.
 const RUNNING_AS_CONTAINER   = process.env.RUNNING_AS_CONTAINER;
 
@@ -17,7 +19,7 @@ app.listen(PORT, () => {
 
     log.info(`App is running at http://localhost:${externalPort}.`);
 
-    swaggerDocs(app, externalPort);
+    swaggerDocs(app, externalPort, LIVE_SERVER_URL);
     
     if (RUNNING_AS_CONTAINER) {
         log.info(`App is running inside of a container. Container port=${PORT}.`);
