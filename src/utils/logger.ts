605e9fa8bae1ef286ef26f79db03a57c9dde80a2
Nikita <nikita@s-i-r-k-a.com>
2024-11-18 00:32:13 +0100

    feat(pino logger config): show `debug` level logs by default.

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,9 +1,15 @@
 import pino from 'pino';
 
+/*
+ * To toggle debug logs visibility in console,
+ * comment the `level` property in pino options object. 
+ */
+
 const log = pino({
     transport: {
         target: 'pino-pretty',
     },
+    level: 'debug',
     base: {
         pid: false,
     },
